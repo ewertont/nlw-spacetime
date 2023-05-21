a2cb1e5a2f6ab3434c2f8b0e5dfdeb239f8ffc5f
Bezerra <ewerton.bezerra@accenture.com>
2023-05-21 11:20:19 -0300

    aula 2

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,29 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
+
+  if (!hasLoadedFonts) {
+    return null;
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-zinc-950">
-      <Text className="text-4xl font-bold text-zinc-50">NLW Spacetime</Text>
+      <Text className="font-body text-4xl font-bold text-zinc-50">
+        NLW Spacetime
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
